59af7de8f624acce02f2920f5dac47b6edef86fe
Rafael Mota <rafaelmota.dev@gmail.com>
2023-01-20 18:49:18 -0300

    Adiciona scrollbar e faz ajustes no link do email

--- js/email-script.js
+++ js/email-script.js
@@ -2,7 +2,7 @@ function myFunction(x) {
   if (x.matches) { // Link Gmail para mobile
     document.getElementById("email").href="mailto:contact@my-website.com?subject=Big%20News&body=Hi,%0D%0A%0D%0A";
   } else {// Link Gmail para demais tamanhos de tela
-    document.getElementById("email").href="https://mail.google.com/mail/u/0/?source=mailto&to=contato@rafaelmotal.dev&fs=1&tf=cm";
+    document.getElementById("email").href="https://mail.google.com/mail/u/0/?source=mailto&to=contato@rafaelmota.dev&fs=1&tf=cm";
   }
 }
 
