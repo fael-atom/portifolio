1982c9da85b00e447189449c663688308ee86201
Rafael Mota <rafaelmota.dev@gmail.com>
2023-01-24 00:10:06 -0300

    Adiciona alterações no menu mobile

---
+++ js/menu-mobile.js
@@ -0,0 +1,12 @@
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+function toggleMobileMenu(menu){
+  menu.classList.toggle('open');
+}
