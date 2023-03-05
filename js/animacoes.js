9eefc28fb635b66a97ad0238698948624b9e2e76
Rafael Mota <rafaelmota.dev@gmail.com>
2023-03-05 17:27:59 -0300

    Cria arquivo separado para animações de scroll

---
+++ js/animacoes.js
@@ -0,0 +1,24 @@
+// Animação para SUBTITULOS
+ScrollReveal().reveal('.subtitulo', {
+  origin: 'left',
+  distance: '100px',
+  duration: 2000
+})
+
+// Animação da INTRODUÇÃO
+ScrollReveal().reveal('.foto-perfil', { scale: 0.15, duration: 2500});
+
+// Animação da EXPERIÊNCIA
+ScrollReveal().reveal('.experiencia-texto', { distance: '200px', origin: 'bottom', duration: 2000 });
+ScrollReveal().reveal('.empresa', { distance: '200px', origin: 'bottom', duration: 2000, delay: 400 });
+
+
+// Animação da FORMAÇÃO
+ScrollReveal().reveal('.formacao-texto', { distance: '200px', origin: 'bottom', duration: 2000 });
+ScrollReveal().reveal('.faculdade-lista', { distance: '200px', origin: 'bottom', duration: 2000, interval: 400 });
+ScrollReveal().reveal('.cursos', { distance: '200px', origin: 'bottom', duration: 2000, interval: 400 });
+ScrollReveal().reveal('.idiomas', { distance: '200px', origin: 'bottom', duration: 2000, interval: 400 });
+
+// Animação do CONTATO
+ScrollReveal().reveal('.footer-descricao', { distance: '200px', origin: 'bottom', duration: 2000 });
+ScrollReveal().reveal('.footer-contato', { distance: '200px', origin: 'bottom', duration: 2000 });
